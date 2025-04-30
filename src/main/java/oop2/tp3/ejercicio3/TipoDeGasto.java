c51b7d7408cf7b9eabfddaf87c63e25eaca530e3
Extends-Object <return.unstring@gmail.com>
2025-04-30 18:20:58 -0300

    First commit

---
+++ src/main/java/oop2/tp3/ejercicio3/TipoDeGasto.java
@@ -0,0 +1,6 @@
+package oop2.tp3.ejercicio3;
+
+public interface TipoDeGasto {
+    public double calcularGastoComida ();
+    public double calcularGastoTotal();
+}
---
+++ src/test/java/oop2/tp3/ejercicio3/ReporteTest.java
@@ -0,0 +1,4 @@
+package oop2.tp3.ejercicio3;
+
+public class ReporteTest {
+}

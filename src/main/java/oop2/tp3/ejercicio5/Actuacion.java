87403e1430e1955811d9dc119b3bb07d2dfbe813
Extends-Object <return.unstring@gmail.com>
2025-06-22 02:54:42 -0300

    -

--- src/main/java/oop2/tp3/ejercicio5/Actuacion.java
+++
@@ -1,4 +0,0 @@
-package oop2.tp3.ejercicio5;
-
-public record Actuacion(String nombreEvento, int numberoEspectadores) {
-}
--- src/main/java/oop2/tp3/ejercicio5/Factura.java
+++
@@ -1,6 +0,0 @@
-package oop2.tp3.ejercicio5;
-
-import java.util.List;
-
-public record Factura(String nombreCliente, List<Actuacion> actuaciones) {
-}
